bc6828f86c211b082d1d5648f6c6cb4b3dc59445
Mark de Vlieger <markdevlieger@users.noreply.github.com>
2021-03-02 13:42:44 +0100

    Added alias for git push

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.ps "push --set-upstream origin HEAD"
 git config --global user.name "Mark de Vlieger"
 git config --global user.email "markdevlieger@users.noreply.github.com"
 git config --global credential.helper store
