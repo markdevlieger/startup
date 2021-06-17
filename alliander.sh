978da676b1a0f7791147e6ec8c8d88f6f59a4068
Mark de Vlieger <markdevlieger@users.noreply.github.com>
2021-06-17 09:36:52 +0200

    Alias to run npm install in subfolders

--- alliander.sh
+++ alliander.sh
@@ -1,2 +1,3 @@
 echo "alias nrd='node run dev'" >> ~/.bash_aliases
 echo "alias nerd='node run debug'" >> ~/.bash_aliases
+echo 'alias nia="ls | grep -E \'js|web|db\' | grep -v \'json\' | xargs -i sh -c \'cd {} && npm install\'"' >> ~/.bash_aliases
