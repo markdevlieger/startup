cda08890a130d6cd5c12bab78d01c69d2d947514
Mark de Vlieger <markdevlieger@users.noreply.github.com>
2021-10-05 16:26:55 +0200

    Update npm scripts Alliander

--- alliander.sh
+++ alliander.sh
@@ -1,3 +1,3 @@
-echo "alias nrd='node run dev'" >> ~/.bash_aliases
-echo "alias nerd='node run debug'" >> ~/.bash_aliases
+echo "alias nrd='npm run dev'" >> ~/.bash_aliases
+echo "alias nerd='npm run debug'" >> ~/.bash_aliases
 echo "alias nia='ls | grep -E \"js|web|db\" | grep -v \"json\" | xargs -i sh -c \"cd {} && npm install\"'" >> ~/.bash_aliases
